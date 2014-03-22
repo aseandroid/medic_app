6fcd43fcc45a6fe13c8a9aa885ffdf93260be897
stefan <stefan@stefan-work>
2014-03-22 19:56:30 +0200

    test

--- src/ase/pdm/pdm_medical/MainActivity.java
+++ src/ase/pdm/pdm_medical/MainActivity.java
@@ -23,7 +23,6 @@ public class MainActivity extends ActionBarActivity {
                     .add(R.id.container, new PlaceholderFragment())
                     .commit();
         }
-        System.out.println('a');
     }
 
 
