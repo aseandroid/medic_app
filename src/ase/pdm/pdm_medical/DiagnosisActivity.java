1e0d58ddf2733a66991787d70023a90fa89fec7b
alin <alingeremia@yahoo.com>
2014-03-31 12:48:20 +0300

    nothing changed, only spaces

--- src/ase/pdm/pdm_medical/DiagnosisActivity.java
+++ src/ase/pdm/pdm_medical/DiagnosisActivity.java
@@ -1,15 +1,10 @@
 package ase.pdm.pdm_medical;
 
 import android.support.v7.app.ActionBarActivity;
-import android.support.v7.app.ActionBar;
-import android.support.v4.app.Fragment;
 import android.os.Bundle;
-import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuItem;
-import android.view.View;
-import android.view.ViewGroup;
-import android.os.Build;
+
 
 public class DiagnosisActivity extends ActionBarActivity {
 
@@ -17,11 +12,10 @@ public class DiagnosisActivity extends ActionBarActivity {
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_diagnosis);
+		
+		
 
-		if (savedInstanceState == null) {
-			getSupportFragmentManager().beginTransaction()
-					.add(R.id.container, new PlaceholderFragment()).commit();
-		}
+		
 	}
 
 	@Override
@@ -47,18 +41,6 @@ public class DiagnosisActivity extends ActionBarActivity {
 	/**
 	 * A placeholder fragment containing a simple view.
 	 */
-	public static class PlaceholderFragment extends Fragment {
-
-		public PlaceholderFragment() {
-		}
-
-		@Override
-		public View onCreateView(LayoutInflater inflater, ViewGroup container,
-				Bundle savedInstanceState) {
-			View rootView = inflater.inflate(R.layout.fragment_diagnosis,
-					container, false);
-			return rootView;
-		}
-	}
+	
 
 }
